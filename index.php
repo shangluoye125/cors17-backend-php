ddde470d53b7b7fe96110dc449a3dd244c8ceef9
shangluoye125 <1157247764@qq.com>
2018-12-27 09:21:21 +0800

    123

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+header("Access-Control-Allow-Origin:http://localhost:63342");
+header("Access-Control-Allow-Credentials:true");
+$username=array('username'=>123);
+echo json_encode($username);
+?>
