c0706e31567ed662fcaeb55832b254719c0cd96a
Administrator <1157247764@>
2018-12-28 09:17:49 +0800

    123

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin:http://localhost:63342");
+header("Access-Control-Allow-Origin:http://localhost:63341");
 header("Access-Control-Allow-Credentials:true");
 $username=array('username'=>123);
 echo json_encode($username);
+
 ?>
