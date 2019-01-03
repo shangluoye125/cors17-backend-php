e4f4252f9f314c53245602be23b9039db5e805f5
Administrator <sa@qq.com>
2019-01-03 09:23:13 +0800

    q

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin:http://localhost:63341");
+header("Access-Control-Allow-Origin:http://10.30.29.76:63341");
 header("Access-Control-Allow-Credentials:true");
-$username=array('username'=>123);
-echo json_encode($username);
+
+$userName=array('userName'=>'123');
+echo json_encode($userName);
 
 
 ?>
