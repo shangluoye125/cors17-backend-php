6c73b224d5de0bb6ca394cada207897410f1d358
Administrator <asd@qq.coom>
2019-01-04 09:17:02 +0800

    123

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 header("Access-Control-Allow-Origin:http://10.30.29.76:63341");
 header("Access-Control-Allow-Credentials:true");
+header("Access-Control-Allow-Header:content_type,xfilecategory,xfilename,xfilesize");
 
-$userName=array('userName'=>'123');
-echo json_encode($userName);
+$data=file_get_contents("php://input");
+$data=json_decode($data);
+$message=array('message'=>'success');
+
+if($data->userName==123&&$data->passWord==123){
+    echo json_encode($message);
+}else{
+    $message['message']='error';
+    echo json_encode($message);
+}
 
 
 ?>
