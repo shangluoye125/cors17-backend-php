e3b9bc37fe775ea07b4bcad9aed6a3c14ef8fb7f
Administrator <wqe@qwe.com>
2019-01-10 08:39:09 +0800

    123

--- login.php
+++ login.php
@@ -1,13 +1,16 @@
 <?php
 header("Access-Control-Allow-Origin:http://10.30.29.76:63341");
 header("Access-Control-Allow-Credentials:true");
-header("Access-Control-Allow-Headers:Content_Type,xfilecategory,xfilename,xfilesize");
-
-$data=file_get_contents("php://input");
+header("Access-Control-Allow-Headers:Content_Type");
+header("Content_Type:text/json");
+$data=file_get_contents("php://input",true);
 $data=json_encode($data);
 $message=array('message'=>"success");
 
+
 if($data->userName==123&&$data->passWord==123){
+    session_start();
+    $_SESSION['name']=$data->userName;
     echo json_encode($message);
 }else{
     $message['message']='error';
--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin:http://10.30.29.76:63341");
 header("Access-Control-Allow-Credentials:true");
-header("Content_Type:text/java");
 
-$username=array('username'=>1);
-echo json_encode($username);
-
+session_start();
+$data=array('message'=>'success');
+echo json_encode($message);
+session_destroy();
 ?>
